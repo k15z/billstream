980f75875abbf7ea6272cf9220e9c421c82a5d8a
juliangay <julian.gay@credprotocol.com>
2024-11-01 17:53:01 -0700

    Create Weather API endpoint

--- api/api/example_service/__init__.py
+++ api/api/example_service/__init__.py
@@ -2,11 +2,13 @@ import uuid
 import fastapi
 from pydantic import BaseModel
 from api._types import RequestPaymentResponse
+import requests
 
 router = fastapi.APIRouter(
     prefix="/example",
     tags=["example"],
 )
+
 
 
 class ExampleRequest(BaseModel):
@@ -32,3 +34,14 @@ async def get_example(id: str) -> ExampleResponse:
     request = id_to_request[id]
     # process request
     return ExampleResponse(message=f"Hello, {request.name}!")
+
+
+@router.get("/weather")
+async def get_weather(city: str) -> ExampleResponse:
+
+    weather_api_key = "f093c2b5da3b49dfa9104019240211"
+    response = requests.get(f"https://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={city}&aqi=no")
+    weather_data = response.json()
+    weather_condition = weather_data['current']['condition']['text']
+    
+    return ExampleResponse(message=f"The weather in {city} is {weather_condition}")
