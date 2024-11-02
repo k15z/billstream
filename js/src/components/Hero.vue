fae62bce3bbdf7d4cef4876859c8b05f9fd0bda6
Kevin Zhang <me@kevz.dev>
2024-11-02 11:14:56 -0700

    Update tagline

--- js/src/components/Hero.vue
+++ js/src/components/Hero.vue
@@ -1,11 +1,11 @@
 <template>
     <div style="height: 100vh; width:100% !important;" class="background">
         <v-container class="d-flex align-center" style="height: 100%;">
-            <v-row style="margin-bottom: 25vh;">
+            <v-row style="margin-bottom: 50vh;">
                 <v-col cols="12">
                     <h1 class="text-h2 text-weight-bold">Agent Toolkit</h1>
                     <h3 class="text-h4 text-weight-light mt-4">
-                        Any tool. Any currency. Any time.
+                        Pay as you go. Any tool, any currency.
                     </h3>
                 </v-col>
             </v-row>
